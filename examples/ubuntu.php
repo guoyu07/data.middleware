3a95aa89b6291517d7a4186f1fe530bb5a43c36e
Abao <lirunfeng@linghit.com>
2016-08-03 10:11:34 +0800

    update ubuntu

---
+++ examples/ubuntu.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ *
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2016
+ *
+ * @link      https://www.github.com/janhuang
+ * @link      http://www.fast-d.cn/
+ */
+
+use FastD\Middleware\DataProvider;
+use FastD\Middleware\Middleware;
+use FastD\Storage\Driver\Redis\Redis;
+use FastD\Storage\Driver\StorageDriver;
+
+include __DIR__ . '/../vendor/autoload.php';
+
+class MySQLMiddleware extends Middleware
+{
+    /**
+     * 数据源, 所有缓存数据最终来源。
+     *
+     * @return mixed
+     */
+    public function dataOriginal()
+    {
+        return 'origin test';
+    }
+}
+
+class RedisProvider extends DataProvider
+{
+    public function getName()
+    {
+        return 'test';
+    }
+
+    /**
+     * @return int
+     */
+    function weight()
+    {
+        return 40;
+    }
+}
+
+$middleware = new MySQLMiddleware();
+$middleware->append(new RedisProvider(new Redis()));
+$data = $middleware->resolve();
+var_dump($data);
